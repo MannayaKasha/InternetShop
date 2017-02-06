d829bbabb21ab6ad9cebdecd11b74e5131b299f9
Pavel Milovidov <mpv7@tut.by>
2017-02-06 23:57:29 +0200

    added some code

--- src/main/java/org/mannayakasha/Main.java
+++ src/main/java/org/mannayakasha/Main.java
@@ -1,8 +1,18 @@
 package org.mannayakasha;
 
-/**
- * Created by Pavel on 06.02.2017.
- */
-public class Main {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Created on 06.02.2017.
+ *
+ * @author Pavel
+ * @version 1.0
+ */
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 }
